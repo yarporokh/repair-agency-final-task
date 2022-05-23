93855881be8d78b19d8873789e94ae3ad80dc687
yarporokh <yaroslavporohnenko@gmail.com>
2022-05-23 13:46:27 +0300

    Removed unnecessary stuff

--- src/main/java/org/example/servlets/ProfileServlet.java
+++ src/main/java/org/example/servlets/ProfileServlet.java
@@ -6,8 +6,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.example.models.User;
 
 import java.io.IOException;
 
@@ -15,8 +13,6 @@ import java.io.IOException;
 public class ProfileServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        User user = (User) session.getAttribute("user");
         RequestDispatcher dis = req.getRequestDispatcher("profile.jsp");
         dis.forward(req, resp);
     }
