4e670e54701295dab87081d90ecb125b9327ceeb
yarporokh <yaroslavporohnenko@gmail.com>
2022-05-27 10:51:21 +0300

    Added encoding filter

---
+++ src/main/java/org/example/filters/EncodingFilter.java
@@ -0,0 +1,32 @@
+package org.example.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = {"/*"}, initParams = {@WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding Param")})
+public class EncodingFilter implements Filter {
+    private String code;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        code = filterConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void destroy() {
+        code = null;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String codeRequest = servletRequest.getCharacterEncoding();
+        if (code != null && !code.equalsIgnoreCase(codeRequest)) {
+            servletRequest.setCharacterEncoding(code);
+            servletRequest.setCharacterEncoding(code);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
