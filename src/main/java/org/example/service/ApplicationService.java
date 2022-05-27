e76655e493c1b56e3821399a79418d155ee20572
yarporokh <yaroslavporohnenko@gmail.com>
2022-05-27 14:58:51 +0300

    Added comments

--- src/main/java/org/example/service/ApplicationService.java
+++ src/main/java/org/example/service/ApplicationService.java
@@ -6,9 +6,15 @@ import org.example.models.Application;
 import java.sql.Date;
 import java.util.List;
 
+/**
+ * The ApplicationService class provides the connection
+ * between servlets and application dao
+ */
 public class ApplicationService {
     private static ApplicationDAO applicationDAO = new ApplicationDAO();
 
+    /**
+     method addApplication adds application in database */
     public static void addApplication(String email, String text) {
         long millis = System.currentTimeMillis();
         Date date = new Date(millis);
@@ -25,14 +31,20 @@ public class ApplicationService {
         applicationDAO.insert(application);
     }
 
+    /**
+     method getUserApplications gets user list of applications */
     public static List<Application> getUserApplications(String email) {
         return applicationDAO.getByEmail(email);
     }
 
+    /**
+     method getAllApplications gets a list of all applications */
     public static List<Application> getAllApplications() {
         return applicationDAO.getAll();
     }
 
+    /**
+     method updateApplication updates an application */
     public static void updateApplication(int id, double price, String serviceman, String paymentStatus, String progress, String responseText) {
         Application application = Application
                 .newBuilder()
@@ -46,10 +58,14 @@ public class ApplicationService {
         applicationDAO.update(application);
     }
 
+    /**
+     method updatePaymentStatus updates a payment status of application to Paid */
     public static void updatePaymentStatus(int id) {
         applicationDAO.updateApplicationPaymentStatus(id, "Paid");
     }
 
+    /**
+     method updateResponseText adds a response text to application */
     public static void updateResponseText(int id, String response) {
         applicationDAO.updateApplicationResponseText(id, response);
     }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -9,9 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The UserService class provides the connection
+ * between servlets and user dao
+ */
 public class UserService {
     private static UserDAO userDAO = new UserDAO();
 
+    /**
+     method addUser adds user in database */
     public static void addUser(String email, String password, String firstName, String lastName) {
         User user = new User();
         user.setEmail(email);
@@ -23,26 +29,38 @@ public class UserService {
         userDAO.insert(user);
     }
 
+    /**
+     method isUserExist returns true if user is in database */
     public static boolean isUserExist(String email) {
         return userDAO.isExist(email);
     }
 
+    /**
+     method loggedUser returns logged user */
     public static User loggedUser(String email, String password) {
         return userDAO.getLoginedUser(email, password);
     }
 
+    /**
+     method updateUser updates user in database */
     public static void updateUser(User user) {
         userDAO.update(user);
     }
 
+    /**
+     method getUsers gets a list of all users */
     public static List getUsers() {
         return userDAO.getAll();
     }
 
+    /**
+     method updateBalanceAndRole updates balance and role of the user */
     public static void updateBalanceAndRole(String email, String role, double balance) {
         userDAO.updateRoleAndBalanceByEmail(email, role, balance);
     }
 
+    /**
+     method getServicemenNamesByEmail gets a list of servicemen full names */
     public static List<String> getServicemenNamesByEmail(List<Application> applicationList) {
         List<String> fullNames = new ArrayList<>();
         for (Application application: applicationList) {
@@ -52,6 +70,8 @@ public class UserService {
         return fullNames;
     }
 
+    /**
+     method getServicemenNamesByEmail gets a list of servicemen emails */
     public static List<String> getServicemenEmailsList() {
         List<User> users = userDAO.getAll();
         List<User> servicemen = users.stream().filter(x->x.getRole() == Role.SERVICEMAN).collect(Collectors.toList());
