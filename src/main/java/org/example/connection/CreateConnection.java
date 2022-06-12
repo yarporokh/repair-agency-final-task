552deed1315f52925c032e5b2fcc26e45299ce21
yarporokh <yaroslavporohnenko@gmail.com>
2022-06-12 19:11:27 +0300

    Add hikari connection pool

--- src/main/java/org/example/connection/CreateConnection.java
+++ src/main/java/org/example/connection/CreateConnection.java
@@ -1,26 +1,29 @@
 package org.example.connection;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class CreateConnection {
 
     private static final String url = "jdbc:mysql://localhost:3306/repair_agency?autoReconnect=true&useSSL=false";
     private static final String user = "root";
     private static final String password = "root";
+    public static final String driver= "com.mysql.jdbc.Driver";
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        ds = new HikariDataSource(config);
+    }
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return DriverManager.getConnection(url, user, password);
+        return ds.getConnection();
     }
 }
